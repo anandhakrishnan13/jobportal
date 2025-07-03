ce1fdec363b06bfdc9b208541a9f9127aa744af1
anandhakrishnan13 <anandhakrishnannair13@gmail.com>
2025-07-03 18:38:08 +0530

    JobPortal

--- Server/server.js
+++ Server/server.js
@@ -11,10 +11,7 @@ import categoryRoutes from './routes/categoryRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000',  // You can allow multiple origins if needed
-  credentials: true,                // Optional: allow cookies if you use them
-}));
+app.use(cors({}));
 app.use(express.json());
 
 // Routes
