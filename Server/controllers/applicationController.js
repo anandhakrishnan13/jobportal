d8aa90bc7779c8800e4ac5fbcf4bd3c8ff803343
anandhakrishnan13 <anandhakrishnannair13@gmail.com>
2025-07-04 05:31:12 +0530

    JobPortal

--- Server/controllers/applicationController.js
+++ Server/controllers/applicationController.js
@@ -50,3 +50,38 @@ export const getAllApplications = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch applications" });
   }
 };
+
+export const updateJob = async (req, res) => {
+  try {
+    const updatedJob = await Job.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedJob) {
+      return res.status(404).json({ error: "Job not found" });
+    }
+
+    res.json(updatedJob);
+  } catch (err) {
+    console.error("Update Job Error:", err);
+    res.status(500).json({ error: "Failed to update job" });
+  }
+};
+
+
+export const deleteJob = async (req, res) => {
+  try {
+    const deletedJob = await Job.findByIdAndDelete(req.params.id);
+
+    if (!deletedJob) {
+      return res.status(404).json({ error: "Job not found" });
+    }
+
+    res.json({ message: "Job deleted successfully" });
+  } catch (err) {
+    console.error("Delete Job Error:", err);
+    res.status(500).json({ error: "Failed to delete job" });
+  }
+};
